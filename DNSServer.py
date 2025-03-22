ccf84ddecafb269fcabe3ae14bb2032b929b0672
shokosobata0619 <sh8444@nyu.edu>
2025-03-22 00:13:56 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -43,9 +43,9 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
-salt = b'Tandon' # Remember it should be a byte-object
-password = 'sh8444@nyu.edu'
-input_string = 'AlwaysWatching'
+salt = 'Tandon' # Remember it should be a byte-object
+password = "sh8444@nyu.edu"
+input_string = "AlwaysWatching"
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
